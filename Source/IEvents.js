366771263a69f0e12b0cc5fdc97f77ff22a7d411
Arieh Glazer <arieh.glazer@gmail.com>
2011-07-23 09:58:26 +0300

    added latching

--- Source/IEvents.js
+++ Source/IEvents.js
@@ -28,12 +28,28 @@ var IEvents = this.IEvents = new Class({
    , fireEvent : function fireEvent(type, args, delay){
         type = removeOn(type);
 
+        if (/:\latched/.test(type)){
+            type.replace(':latched','');
+
+            if (!this.$latched) this.$latched = {type:args};    
+            else this.$latched[type] = {args:args, delay:delay || 1};
+        }
+
         if (this.cancellables && this.cancellables.contains(type)){
             for (var key in this.$events[type]) if (this.$events[type].hasOwnProperty(key)){
                 if (!this.$events[type][key].apply(this,Array.from(args))) return false;
             }   
             return this;
         } else return this.parent(type,args,delay || 1);    
+   }
+   , addEvent : function(type,fn){
+        type = removeOn(type);
+
+        if (this.$latched && this.$latched[type]){
+            fn.delay(this.$latched[delay], this, this.$latched[args]);    
+        }
+
+        return this.parent(type,fn);
    }
 });
 
